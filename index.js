8e82530ff9aa773ef7174731093451bf1306c589
IloveGreeny <greenxd23@gmail.com>
2022-04-13 21:59:07 +0400

    second commit

--- index.js
+++ index.js
@@ -10,3 +10,32 @@ let grid;
 let cols;
 let rows;
 let resolution = 10;
+
+function setup() {
+    createCanvas(2000, 2000);
+    cols = width / resolution;
+    rows = height / resolution;
+    grid = make2DArray(cols, rows);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            grid[i][j] = floor(random(2));
+        }
+    }
+}
+
+function draw() {
+    background(0);
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows; j++) {
+            let x = i * resolution;
+            let y = j * resolution;
+            if (grid[i][j] === 1) {
+                fill(255);
+                stroke(0);
+                rect(x, y, resolution - 1, resolution - 1);
+            }
+        }
+    }
+
+    let next = make2DArray(cols, rows);
+}

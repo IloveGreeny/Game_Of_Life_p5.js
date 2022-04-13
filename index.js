51896c62a5b4bdc7c710e7cb23472107e4a77950
IloveGreeny <greenxd23@gmail.com>
2022-04-13 22:09:13 +0400

    fourth commit

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ let rows;
 let resolution = 10;
 
 function setup() {
-    createCanvas(2000, 2000);
+    createCanvas(600, 400);
     cols = width / resolution;
     rows = height / resolution;
     grid = make2DArray(cols, rows);
@@ -38,9 +38,6 @@ function draw() {
     }
 
     let next = make2DArray(cols, rows);
-
-
-
     for (let i = 0; i < cols; i++) {
         for (let j = 0; j < rows; j++) {
             let state = grid[i][j];
@@ -59,18 +56,15 @@ function draw() {
     grid = next;
 }
 
-
-
 function countNeighbors(grid, x, y) {
-        let sum = 0;
-        for (let i = -1; i < 2; i++) {
-            for (let j = -1; j < 2; j++) {
-                let col = (x + i + cols) % cols;
-                let row = (y + j + rows) % rows;
-                sum += grid[col][row];
-            }
+    let sum = 0;
+    for (let i = -1; i < 2; i++) {
+        for (let j = -1; j < 2; j++) {
+            let col = (x + i + cols) % cols;
+            let row = (y + j + rows) % rows;
+            sum += grid[col][row];
         }
-        sum -= grid[x][y];
-        return sum;
     }
+    sum -= grid[x][y];
+    return sum;
 }
